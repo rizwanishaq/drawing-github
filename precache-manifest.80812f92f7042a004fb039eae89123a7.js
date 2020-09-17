eeca5318abf9a724d1d8f08d39dea31e99d05e0c
rizwanishaq <rizwanishaq@gmail.com>
2020-09-17 17:37:25 +0200

    Updates

---
+++ precache-manifest.80812f92f7042a004fb039eae89123a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99d40b149eb6c2fa37f4008e2f3227c",
+    "url": "/drawing-github/index.html"
+  },
+  {
+    "revision": "58aad6054da9b634c4c8",
+    "url": "/drawing-github/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7f6183533b7ea87aad66",
+    "url": "/drawing-github/static/js/2.6636db17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drawing-github/static/js/2.6636db17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58aad6054da9b634c4c8",
+    "url": "/drawing-github/static/js/main.38ca3e5c.chunk.js"
+  },
+  {
+    "revision": "25164b32d0100da9802a",
+    "url": "/drawing-github/static/js/runtime-main.bd6b02fd.js"
+  }
+]);
